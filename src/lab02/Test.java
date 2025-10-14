ee8c8ab25ef181e72860bf68b3613bdb241c24e7
mariooooo3 <afrasineimario@gmail.com>
2025-10-14 17:00:45 +0300

    init commit

---
+++ src/lab02/Test.java
@@ -0,0 +1,13 @@
+package lab02;
+
+public class Test {
+	private String tipString;
+	
+	Test(String tipString)
+	{
+		this.tipString = tipString;
+		System.out.println("Constructor Test" + tipString);
+	}
+	
+	
+}
---
+++ src/lab02/TestProb2.java
@@ -0,0 +1,16 @@
+package lab02;
+
+public class TestProb2 {
+	
+	public TestProb2()
+	{
+		System.out.println("Cons fara argumente");
+		
+	}
+	
+	public static void main(String[] argc)
+	{
+		Ini i1 = new Ini("i1");
+		Ini i2 = new Ini("i2");
+	}
+}
