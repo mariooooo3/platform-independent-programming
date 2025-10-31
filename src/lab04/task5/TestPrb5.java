0daac06c1f9296cbfdb785b4585e426c41845c6a
mariooooo3 <afrasineimario@gmail.com>
2025-10-31 18:00:27 +0200

    init commit

---
+++ src/lab04/task5/TestPrb5.java
@@ -0,0 +1,15 @@
+package lab04.task5;
+
+
+public class TestPrb5 {
+    TestPrb5() {
+        System.out.println("Constructor TestPrb5");
+    }
+    public static void main(String[] args) {
+        ClasaExterioara cx = new ClasaExterioara();
+        ClasaExterioara.ClasaIn ci = new ClasaExterioara.ClasaIn();
+        ci.metoda3();
+        ClasaExterioara.ClasaInStatica cis = new ClasaExterioara.ClasaInStatica();
+        cis.metoda1();
+    }
+}
---
+++ src/lab05/Prb2.java
@@ -0,0 +1,20 @@
+package lab05;
+import java.util.Hashtable;
+public class Prb2 {
+    public static void main(String[] args)
+    {
+        String nr_pi_100 = "3.1415926535897932384626433832795028841971693993"
+
+                +"751058209749445923078164062862089986280348253421170679";
+        Hashtable<String, Integer> hash = new Hashtable<String, Integer>();
+
+
+        String nr = nr_pi_100.replace(".", "");
+
+        for(int i=0;i<nr.length();i++)
+        {
+            String douaCifre = nr.substring(i, i+2);
+
+        }
+    }
+}
